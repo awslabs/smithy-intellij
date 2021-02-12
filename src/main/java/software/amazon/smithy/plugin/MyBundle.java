96890214232b8ddacccb491f5ed9f2bbc9bea548
Chase Coalwell <chasecoa@amazon.com>
2021-02-11 17:25:29 -0800

    Add checkstyle

--- src/main/java/software/amazon/smithy/plugin/MyBundle.java
+++ src/main/java/software/amazon/smithy/plugin/MyBundle.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package software.amazon.smithy.plugin;
 
 import com.intellij.AbstractBundle;
@@ -5,11 +20,13 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-public class MyBundle extends AbstractBundle {
+public final class MyBundle extends AbstractBundle {
     public static final MyBundle INSTANCE = new MyBundle();
     @NonNls public static final String BUNDLE = "message.MyBundle";
 
-    private MyBundle() { super(BUNDLE); }
+    private MyBundle() {
+        super(BUNDLE);
+    }
 
     @NotNull
     public static String message(@PropertyKey(resourceBundle = BUNDLE) String key, Object... params) {
